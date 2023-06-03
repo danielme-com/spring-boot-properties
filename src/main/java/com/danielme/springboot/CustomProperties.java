7217b078832564bd585d902f1b035feeb520f145
dani <danielme_com@yahoo.com>
2023-06-03 16:54:14 +0200

    upadte Spring Boot 3 (Java 17)

--- src/main/java/com/danielme/springboot/CustomProperties.java
+++ src/main/java/com/danielme/springboot/CustomProperties.java
@@ -3,7 +3,7 @@ package com.danielme.springboot;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
-import javax.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotBlank;
 
 @Validated
 @ConfigurationProperties("custom")
--- src/main/java/com/danielme/springboot/ServersProperties.java
+++ src/main/java/com/danielme/springboot/ServersProperties.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.validation.annotation.Validated;
 
-import javax.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotBlank;
 
 @Configuration
 @Validated
