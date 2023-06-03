30555ff2939d58674938a0eb71366c4a66c0aa1a
dani <danielme_com@yahoo.com>
2023-06-03 16:54:57 +0200

    @EnableConfigurationProperties example (commented)

--- src/main/java/com/danielme/springboot/SpringBootPropertiesApplication.java
+++ src/main/java/com/danielme/springboot/SpringBootPropertiesApplication.java
@@ -3,10 +3,12 @@ package com.danielme.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource("classpath:custom.properties")
+//@EnableConfigurationProperties({CustomProperties.class})
 @ConfigurationPropertiesScan
 public class SpringBootPropertiesApplication {
 
